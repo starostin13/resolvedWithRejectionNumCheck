58e1de0ecf634562f77041759c21a2c1ac52f578
starostin13 <starostin13@outlook.com>
2023-06-27 15:38:10 +0300

    pack promise to array and add catches

--- promise.test.js
+++ promise.test.js
@@ -1,18 +1,18 @@
 const getPromise = require('./promise');
 const resolvedWithRejectionNumCheck = require('./resolvedWithRejectionNumCheck.js');
 
-test('resolves if amount of rejected promises from promisesToHandle is not greater than the maxRejectedNum value', async () => {
+test('resolves if amount of rejected promises from promisesToHandle is not greater than 10', async () => {
   var random = Math.floor(Math.random() * 10);
-
+  let promises = [...new Array(random)];
   console.log('brfore promises created');
-  //const promises = [...new Array(random)].map(()=> getPromise());
-  //const promises = () => getPromise(); //WORK CORRECTLY
+  for(let i=0; i < random; i++) {
+    promises[i] = () => getPromise();
+  }
+  
   console.log('brfore promises then-ed');
   var result  = resolvedWithRejectionNumCheck(promises, 10).then(data => {
     console.log(`Data? ${data}`);
-    expect(data).toBe('Resolved');
-  }).catch(err => {
-    expect(1).toBe(2);
+    expect(data).toMatch('acceptable');
   });
   
   return result;
--- resolvedWithRejectionNumCheck.js
+++ resolvedWithRejectionNumCheck.js
@@ -1,23 +1,21 @@
-function resolvedWithRejectionNumCheck(promisesToHandle, maxRejectedNum) {
-    var i = 0;
+async function resolvedWithRejectionNumCheck(promisesToHandle, maxRejectedNum) {
+    var resolved = 0, rejected = 0;
     console.log(`Here is a ${promisesToHandle.length} promises`);
 
-    // try {
-    //     promisesToHandle.all.then((data) => {
-    //         if (data == 'reject') {
-    //             i++;
-    //             console.log(`${i} times rejected`);
-    //         }
-    //     });
+    await promisesToHandle.map(prms => prms()
+    .then(value => {
+        console.log(`Then ${value}`);
+        resolved++;
+    })
+    .catch(err => {
+        console.log(`CATCH: ${err}`);
+        rejected++
+    }));
 
-    // } catch {
-    //     i++;
-    //     console.log(`${i}s times rejected`);
-    // }
+    console.log(`Resolve: ${resolved}; Rejected: ${rejected}`);
 
     return new Promise((resolve, reject) => {
-        //if (i < maxRejectedNum) {
-        if (true) {
+        if (promisesToHandle.length - resolved <= maxRejectedNum) {        
             resolve('number of rejected promises acceptable for the limit');
         } else {
             reject('number of rejected promises over the limit');
