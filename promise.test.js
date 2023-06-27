68dae6dc4a133a86be5ccc2fd3daa240efbf220b
starostin13 <starostin13@outlook.com>
2023-06-27 21:12:00 +0300

    Add case maxRejectedNum is -1

--- promise.test.js
+++ promise.test.js
@@ -1,6 +1,21 @@
 const getPromise = require('./promise');
 const resolvedWithRejectionNumCheck = require('./resolvedWithRejectionNumCheck.js');
 
+test('maxRejectedNum is -1', async () => {
+  var random = Math.floor(Math.random() * 10);
+  let promises = [...new Array(random)];
+  
+  for(let i=0; i < random; i++) {
+    promises[i] = () => getPromise();
+  }
+  
+  var result  = resolvedWithRejectionNumCheck(promises, -1).then(data => {
+    expect(data).toMatch('acceptable');
+  });
+  
+  return result;
+});
+
 test('maxRejectedNum is null', async () => {
   var random = Math.floor(Math.random() * 10);
   let promises = [...new Array(random)];
