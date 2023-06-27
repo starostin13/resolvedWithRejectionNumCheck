19c909e9d5e9399ac19b88f47be545de1d10d7df
starostin13 <starostin13@outlook.com>
2023-06-27 17:42:23 +0300

    maxRejectedNum is null case added

--- promise.test.js
+++ promise.test.js
@@ -1,10 +1,25 @@
 const getPromise = require('./promise');
 const resolvedWithRejectionNumCheck = require('./resolvedWithRejectionNumCheck.js');
 
-test('resolves if amount of rejected promises from promisesToHandle is not greater than 10', async () => {
+test('maxRejectedNum is null', async () => {
   var random = Math.floor(Math.random() * 10);
   let promises = [...new Array(random)];
   
+  for(let i=0; i < random; i++) {
+    promises[i] = () => getPromise();
+  }
+  
+  var result  = resolvedWithRejectionNumCheck(promises, null).then(data => {
+    expect(data).toMatch('acceptable');
+  });
+  
+  return result;
+});
+
+test('resolves if amount of rejected promises from promisesToHandle is not greater than 10', async () => {
+  var random = Math.floor(Math.random() * 10);
+  let promises = [...new Array(random)];
+
   for(let i=0; i < random; i++) {
     promises[i] = () => getPromise();
   }
